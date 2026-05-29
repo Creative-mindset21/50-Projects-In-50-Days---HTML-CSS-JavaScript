84489aa0c7173cf9027a8970659c12b21499db21
Creative-mindset21 <tobiawotoye03@gmail.com>
2026-05-29 14:32:56 +0100

    Completed Form Wave Animation

---
+++ Day 8 - Form Wave Animation/script.js
@@ -0,0 +1,21 @@
+const labels = document.querySelectorAll(".form-control label");
+
+labels.forEach((label) => {
+  label.innerHTML = label.innerText
+    .split("")
+    .map(
+      (letter, i) =>
+        `<span style="transition-delay: ${i * 50}ms">${letter}</span>`,
+    )
+    .join("");
+});
+
+// labels.forEach((label) => {
+//   label.innerHTML = label.innerText
+//     .split("")
+//     .map(
+//       (letter, i) =>
+//         `<span style="transition-delay: ${i * 50}ms">${letter}</span>`,
+//     )
+//     .join("");
+// });
